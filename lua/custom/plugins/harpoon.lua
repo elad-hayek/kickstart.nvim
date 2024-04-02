1361e1c1aba09ac4303d1f176905c4ae16a6d893
eladh <elad3283@gmail.com>
2024-04-02 22:19:43 +0300

    change harpoon mappings

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -31,20 +31,21 @@ return {
       end
 
       vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
-      vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+      vim.keymap.set("n", "<leader>e", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-      vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-      vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-      vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-      vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+      vim.keymap.set("n", "<M-1>", function() harpoon:list():select(1) end)
+      vim.keymap.set("n", "<M-2>", function() harpoon:list():select(2) end)
+      vim.keymap.set("n", "<M-3>", function() harpoon:list():select(3) end)
+      vim.keymap.set("n", "<M-4>", function() harpoon:list():select(4) end)
+      vim.keymap.set("n", "<M-5>", function() harpoon:list():select(5) end)
+      vim.keymap.set("n", "<M-6>", function() harpoon:list():select(6) end)
+      vim.keymap.set("n", "<M-7>", function() harpoon:list():select(7) end)
+      vim.keymap.set("n", "<M-8>", function() harpoon:list():select(8) end)
+      vim.keymap.set("n", "<M-9>", function() harpoon:list():select(9) end)
 
       -- Toggle previous & next buffers stored within Harpoon list
-      vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-      vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
-
-      -- Toggle previous & next buffers stored within Harpoon list
-      vim.keymap.set('n', '<C-S-P>', function() harpoon:list():prev() end)
-      vim.keymap.set('n', '<C-S-N>', function() harpoon:list():next() end)
+      vim.keymap.set("n", "<M-[>", function() harpoon:list():prev() end)
+      vim.keymap.set("n", "<M-]>", function() harpoon:list():next() end)
     end,
   },
 }

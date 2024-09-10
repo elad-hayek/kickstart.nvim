85fa88e607a06763db10320a32d84057e56cbe60
Elad Hayek <elad3283@gmail.com>
2024-09-10 21:25:56 +0300

    create branch for leptop

--- init.lua
+++ init.lua
@@ -548,7 +548,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         clangd = {
-          cmd =  { 'clangd', '--offset-encoding=utf-16' },
+          cmd = { 'clangd', '--offset-encoding=utf-16' },
         },
         -- gopls = {},
         -- pyright = {},
@@ -562,6 +562,8 @@ require('lazy').setup({
         tsserver = {},
         --
         angularls = {},
+        html = {},
+        cssls = {},
 
         lua_ls = {
           -- cmd = {...},
@@ -611,23 +613,23 @@ require('lazy').setup({
   },
 
   { -- Autoformat
-    --'stevearc/conform.nvim',
-    --opts = {
-    --  notify_on_error = false,
-    --  format_on_save = {
-    --    timeout_ms = 500,
-    --    lsp_fallback = true,
-    --  },
-    --  formatters_by_ft = {
-    --    lua = { 'stylua' },
-    --    -- Conform can also run multiple formatters sequentially
-    --    -- python = { "isort", "black" },
-    --    --
-    --    -- You can use a sub-list to tell conform to run *until* a formatter
-    --    -- is found.
-    --    -- javascript = { { "prettierd", "prettier" } },
-    --  },
-    --},
+    'stevearc/conform.nvim',
+    opts = {
+      notify_on_error = false,
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        -- Conform can also run multiple formatters sequentially
+        -- python = { "isort", "black" },
+        --
+        -- You can use a sub-list to tell conform to run *until* a formatter
+        -- is found.
+        -- javascript = { { "prettierd", "prettier" } },
+      },
+    },
   },
 
   { -- Autocompletion
